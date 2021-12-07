5b41757e8e072f861ba1f1a041b761c240768b2d
Henrik Bergqvist <henrik.bergqvist@alektumgroup.com>
2021-12-07 10:17:25 +0100

    Refactor

--- src/AdventOfCode2021/Days/Day7.fs
+++ src/AdventOfCode2021/Days/Day7.fs
@@ -8,7 +8,7 @@ let Problem1 (input : string) =
     let checks = [crabs |> Array.min .. crabs |> Array.max]
 
     let totalMoves c (d : int) =
-        c |> Array.map (fun e -> System.Math.Abs(e - d)) |> Array.sum
+        c |> Array.sumBy (fun e -> (e - d) |> abs)
 
     checks |> List.map (fun e -> (e, totalMoves crabs e)) |> List.sortBy snd |> List.head |> snd
 
@@ -17,11 +17,11 @@ let Problem2 (input : string) =
         
     let crabs = input.Split(',') |> Array.map int
 
-    let costs = [1 .. crabs |> Array.max] |> List.scan (+) 0
+    let costs = [|1 .. crabs |> Array.max|] |> Array.scan (+) 0
     
     let checks = [crabs |> Array.min .. crabs |> Array.max]
     
     let totalMoves c (d : int) =
-        c |> Array.map (fun e -> costs[System.Math.Abs(e - d)]) |> Array.sum
+        c |> Array.sumBy (fun e -> costs[(e - d) |> abs])
     
     checks |> List.map (fun e -> (e, totalMoves crabs e)) |> List.sortBy snd |> List.head |> snd
