4aa71d128ea3631caf872842a07ad6bbe6a52f7a
Henrik Bergqvist <henrik.bergqvist@alektumgroup.com>
2021-12-07 11:07:21 +0100

    Refactor

--- src/AdventOfCode2021/Days/Day7.fs
+++ src/AdventOfCode2021/Days/Day7.fs
@@ -7,21 +7,21 @@ let Problem1 (input : string) =
 
     let checks = [|crabs |> Array.min .. crabs |> Array.max|]
 
-    let totalMoves c (d : int) =
-        c |> Array.sumBy (fun e -> (e - d) |> abs)
+    let totalMoves (d : int) =
+        crabs |> Array.sumBy (fun e -> (e - d) |> abs)
         
-    checks |> Array.map (totalMoves crabs) |> Array.min
+    checks |> Array.map totalMoves |> Array.min
 
     
 let Problem2 (input : string) =
-        
+    
     let crabs = input.Split(',') |> Array.map int
 
-    let costs = [|1 .. crabs |> Array.max|] |> Array.scan (+) 0
+    let costs = [|1 .. (crabs |> Array.max) - (crabs |> Array.min)|] |> Array.scan (+) 0
     
     let checks = [|crabs |> Array.min .. crabs |> Array.max|]
     
-    let totalMoves c (d : int) =
-        c |> Array.sumBy (fun e -> costs[(e - d) |> abs])
+    let totalMoves (d : int) =
+        crabs |> Array.sumBy (fun e -> costs[(e - d) |> abs])
     
-    checks |> Array.map (totalMoves crabs) |> Array.min
+    checks |> Array.map totalMoves |> Array.min
