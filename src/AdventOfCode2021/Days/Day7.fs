f9156f7cc85b8feab0b998844304b1da38fa2173
Henrik Bergqvist <henrik.bergqvist@alektumgroup.com>
2021-12-07 10:55:32 +0100

    refactor

--- src/AdventOfCode2021/Days/Day7.fs
+++ src/AdventOfCode2021/Days/Day7.fs
@@ -5,12 +5,12 @@ let Problem1 (input : string) =
     
     let crabs = input.Split(',') |> Array.map int
 
-    let checks = [crabs |> Array.min .. crabs |> Array.max]
+    let checks = [|crabs |> Array.min .. crabs |> Array.max|]
 
     let totalMoves c (d : int) =
         c |> Array.sumBy (fun e -> (e - d) |> abs)
-
-    checks |> List.map (fun e -> (e, totalMoves crabs e)) |> List.sortBy snd |> List.head |> snd
+        
+    checks |> Array.map (totalMoves crabs) |> Array.min
 
     
 let Problem2 (input : string) =
@@ -19,9 +19,9 @@ let Problem2 (input : string) =
 
     let costs = [|1 .. crabs |> Array.max|] |> Array.scan (+) 0
     
-    let checks = [crabs |> Array.min .. crabs |> Array.max]
+    let checks = [|crabs |> Array.min .. crabs |> Array.max|]
     
     let totalMoves c (d : int) =
         c |> Array.sumBy (fun e -> costs[(e - d) |> abs])
     
-    checks |> List.map (fun e -> (e, totalMoves crabs e)) |> List.sortBy snd |> List.head |> snd
+    checks |> Array.map (totalMoves crabs) |> Array.min
