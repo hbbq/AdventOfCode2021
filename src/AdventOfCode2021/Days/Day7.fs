482e83241833347019222565674f3d7d55ac61c1
Henrik Bergqvist <henrik.bergqvist@alektumgroup.com>
2021-12-07 12:58:55 +0100

    Recode

--- src/AdventOfCode2021/Days/Day7.fs
+++ src/AdventOfCode2021/Days/Day7.fs
@@ -4,24 +4,26 @@
 let Problem1 (input : string) =
     
     let crabs = input.Split(',') |> Array.map int
-
-    let checks = [|crabs |> Array.min .. crabs |> Array.max|]
+    
+    let min = crabs |> Array.min
+    let max = crabs |> Array.max
 
     let totalMoves (d : int) =
         crabs |> Array.sumBy (fun e -> (e - d) |> abs)
         
-    checks |> Array.map totalMoves |> Array.min
+    [|min .. max|] |> Array.minBy totalMoves
 
     
 let Problem2 (input : string) =
     
     let crabs = input.Split(',') |> Array.map int
 
-    let costs = [|1 .. (crabs |> Array.max) - (crabs |> Array.min)|] |> Array.scan (+) 0
-    
-    let checks = [|crabs |> Array.min .. crabs |> Array.max|]
-    
-    let totalMoves (d : int) =
+    let min = crabs |> Array.min
+    let max = crabs |> Array.max
+
+    let costs = [|1 .. max - min|] |> Array.scan (+) 0
+            
+    let totalFuel d =
         crabs |> Array.sumBy (fun e -> costs[(e - d) |> abs])
     
-    checks |> Array.map totalMoves |> Array.min
+    [|min .. max|] |> Array.minBy totalFuel
