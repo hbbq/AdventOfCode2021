fbcc3deda6791dc984ed1d07d41790ee1d8c8ea1
Henrik Bergqvist <henrik.bergqvist@alektumgroup.com>
2021-12-10 14:43:40 +0100

    recode

--- src/AdventOfCode2021/Days/Day10.fs
+++ src/AdventOfCode2021/Days/Day10.fs
@@ -4,32 +4,24 @@
 let Problem1 (input : string) =
 
     let lines = input |> Common.Lines
-
-    let pairs = 
-        [|
-            ('(', ')', 3)
-            ('[', ']', 57)
-            ('{', '}', 1197)
-            ('<', '>', 25137)
-        |]
-
+        
     let checkIllegal (line : string) = 
-        let stack = new System.Collections.Generic.Stack<char>()
-        let check =
-            [|
-                for c in line do
-                    let pair = pairs |> Array.tryFind (fun (a, _, _) -> a = c)
-                    match pair with
-                    | Some (_, a, _) -> stack.Push(a)
-                    | None -> 
-                        if stack.Count > 0 then
-                            let t = stack.Pop()
-                            if c <> t then c
-            |]
-        if check.Length = 0 then
+        let rec remove (l : string) =
+            let nl = l.Replace("()", "").Replace("<>", "").Replace("{}", "").Replace("[]", "")
+            if nl.Length = l.Length then 
+                nl.Replace("(","").Replace("[","").Replace("{","").Replace("<","") 
+            else 
+                remove nl
+        let ckd = remove line
+        if ckd.Length = 0 then 
             0
         else
-           pairs |> Array.find (fun (_, a, _) -> a = check[0]) |> (fun (_, _, a) -> a)
+            match ckd[0] with
+            | ')' -> 3
+            | ']' -> 57
+            | '}' -> 1197
+            | '>' -> 25137
+            | _ -> 0
 
     lines |> Array.map checkIllegal |> Array.reduce (+)
                 
@@ -38,40 +30,33 @@ let Problem2 (input : string) =
 
     let lines = input |> Common.Lines
 
-    let pairs = 
-        [|
-            ('(', ')', 1L)
-            ('[', ']', 2L)
-            ('{', '}', 3L)
-            ('<', '>', 4L)
-        |]
-
     let remaining (line : string) = 
-        let stack = new System.Collections.Generic.Stack<char>()
-        let check =
-            [|
-                for c in line do
-                    let pair = pairs |> Array.tryFind (fun (a, _, _) -> a = c)
-                    match pair with
-                    | Some (_, a, _) -> stack.Push(a)
-                    | None -> 
-                        if stack.Count > 0 then
-                            let t = stack.Pop()
-                            if c <> t then c
-            |]
-        if check.Length > 0 then
+        let rec remove (l : string) =
+            let nl = l.Replace("()", "").Replace("<>", "").Replace("{}", "").Replace("[]", "")
+            if nl.Length = l.Length then 
+                nl
+            else 
+                remove nl
+        let ckd = remove line
+        if ckd.Contains("]") || ckd.Contains("}") || ckd.Contains(")") || ckd.Contains(">") then
             [||]
         else
-            stack.ToArray()
+            ckd.ToCharArray()
 
     let lineScore (r : char[]) =
         r 
-        |> Array.map (fun e -> pairs |> Array.find (fun (_, a, _) -> a = e) |> (fun (_, _, a) -> a))
+        |> Array.rev
+        |> Array.map (fun e -> 
+            match e with
+            | '(' -> 1L
+            | '[' -> 2L
+            | '{' -> 3L
+            | '<' -> 4L
+            | _ -> 0l
+        )
         |> Array.reduce (fun a b -> a * 5L + b)
-
 
     let scores = lines |> Array.map remaining |> Array.where (fun e -> e.Length > 0) |> Array.map lineScore |> Array.sort
 
     scores[(scores.Length-1)/2]
-                
-
+               
