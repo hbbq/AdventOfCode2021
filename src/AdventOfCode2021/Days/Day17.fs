6cb52ebba8651272ae7db634065193dabbd0cd3f
Henrik Bergqvist <henrik.bergqvist@alektumgroup.com>
2021-12-17 14:36:17 +0100

    refac

--- src/AdventOfCode2021/Days/Day17.fs
+++ src/AdventOfCode2021/Days/Day17.fs
@@ -12,22 +12,16 @@ let Problem2 (input : string) =
 
     let minxv = xmin * 2 |> float |> sqrt |> floor |> int
 
-    let valids =
-        [|
-            for xv in [minxv .. xmax] do 
-                for yv in [ymin .. -1-ymin] do
-                    let rec step x y xv yv =
-                        let nx = x + xv
-                        let ny = y + yv
-                        if nx >= xmin && nx <= xmax && ny >= ymin && ny <= ymax then
-                            true
-                        elif nx > xmax || ny < ymin then
-                            false
-                        else
-                            let nxv = if xv = 0 then 0 else xv - 1
-                            let nyv = yv - 1
-                            step nx ny nxv nyv
-                    if (step 0 0 xv yv) then (xv, yv)
-        |]
+    let rec step x y (xv, yv) =
 
-    valids |> Array.length
+                let nx = x + xv
+                let ny = y + yv
+                let hit = nx >= xmin && nx <= xmax && ny >= ymin && ny <= ymax
+                let miss = nx > xmax || ny < ymin
+
+                if not(hit || miss) then
+                    step nx ny ((max (xv - 1) 0), (yv - 1))
+                else
+                    hit
+
+    List.allPairs [minxv .. xmax] [ymin .. -1-ymin] |> List.where (step 0 0) |> List.length
