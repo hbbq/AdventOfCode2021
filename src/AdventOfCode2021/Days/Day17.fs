45ee0bb38995fba5345bd05fa5a615bf1c7dfa9c
Henrik Bergqvist <henrik.bergqvist@alektumgroup.com>
2021-12-17 14:12:42 +0100

    refac

--- src/AdventOfCode2021/Days/Day17.fs
+++ src/AdventOfCode2021/Days/Day17.fs
@@ -10,28 +10,24 @@ let Problem2 (input : string) =
 
     let [|xmin;xmax;ymin;ymax|] = input.Split('=','.',',') |> Array.where Common.IsNumeric |> Array.map int
 
-    let minxv = 
-        {1..xmin}
-        |> Seq.scan (+) 0
-        |> Seq.mapi (fun i e -> (i, e))
-        |> Seq.find (fun (_, e) -> e >= xmin)
-        |> fst
+    let minxv = xmin * 2 |> float |> sqrt |> floor |> int
 
     let valids =
         [|
             for xv in [minxv .. xmax] do 
                 for yv in [ymin .. -1-ymin] do
-                    let mutable px = 0
-                    let mutable py = 0
-                    let mutable txv = xv
-                    let mutable tyv = yv
-                    while px < xmax && py > ymin && not(px >= xmin && px <= xmax && py >= ymin && py <= ymax) do
-                        px <- px + txv
-                        py <- py + tyv
-                        if txv > 0 then txv <- txv - 1
-                        tyv <- tyv - 1
-                    if px >= xmin && px <= xmax && py >= ymin && py <= ymax then (xv, yv)
-
+                    let rec step x y xv yv =
+                        let nx = x + xv
+                        let ny = y + yv
+                        if nx >= xmin && nx <= xmax && ny >= ymin && ny <= ymax then
+                            true
+                        elif nx > xmax || ny < ymin then
+                            false
+                        else
+                            let nxv = if xv = 0 then 0 else xv - 1
+                            let nyv = yv - 1
+                            step nx ny nxv nyv
+                    if (step 0 0 xv yv) then (xv, yv)
         |]
 
     valids |> Array.length
